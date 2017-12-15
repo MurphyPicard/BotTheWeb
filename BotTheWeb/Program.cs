3c355f905f5ca93ef8eeaa194634930e0b414a7d
Ara <ayaghsizian@gmail.com>
2017-12-14 20:25:08 -0500

    it works and searches google

--- BotTheWeb/Program.cs
+++ BotTheWeb/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace BotTheWeb
@@ -15,8 +16,15 @@ namespace BotTheWeb
             
             var chromeDriver = new ChromeDriver();
 
-            chromeDriver.Navigate().GoToUrl("http://reddit.com");
+            chromeDriver.Navigate().GoToUrl("https://www.google.com");
 
+            chromeDriver.FindElementById("lst-ib").Click(); // clicking 
+            chromeDriver.Keyboard.SendKeys("Litecoin");     // typing
+            chromeDriver.Keyboard.SendKeys(Keys.Enter);     // searching
+
+            /* 
+            xpath: //*[@id="thing_t3_77ofz0"]/div[2]/div/p[1]/a
+            */
 
         }
     }
